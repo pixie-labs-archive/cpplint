6ce837aac17faaec524a7e566db86ecd283fe668
Omid Azizi <oazizi@pixielabs.ai>
2019-01-29 10:37:22 -0800

    Allow std::chrono and std::thread

--- cpplint.py
+++ cpplint.py
@@ -6014,8 +6014,6 @@ def FlagCxx11Features(filename, clean_lines, linenum, error):
                                       'fenv.h',
                                       'future',
                                       'mutex',
-                                      'thread',
-                                      'chrono',
                                       'ratio',
                                       'regex',
                                       'system_error',
